46ff5609920617fc6dc797dd89dc0f66804865d3
Noel Arzola Jr <noel.arzola@gmail.com>
2021-05-09 06:57:55 -0400

    Updated gitignore

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const overlayContent = document.querySelector(".overlay-content a");
+
+/* Open */
+function openNav() {
+  document.querySelector("#myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.querySelector("#myNav").style.height = "0%";
+}
